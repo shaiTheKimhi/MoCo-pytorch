be5acfa55d5d82177ac7d1cf0dfb2ac415c14078
shaiTheKimhi <shaikimhi221199@gmail.com>
2021-12-22 21:18:34 +0200

    working data loader

--- dataset.py
+++ dataset.py
@@ -2,6 +2,11 @@ from PIL import Image
 import pandas as pd
 import torch
 import os
+import torchvision
+from torchvision import transforms
+
+import torch
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 classes = {'n01440764': 0,
            'n02102040': 1,
@@ -13,21 +18,80 @@ classes = {'n01440764': 0,
            'n03425413': 7,
            'n03445777': 8,
            'n03888257': 9}
+NUM_CHANNELS = 3
+
+
+def get_statistics(dataset):
+    bs = 1 #batch size
+    marr = torch.zeros(5, NUM_CHANNELS) #3 is channels number
+    stdarr = torch.zeros(5, NUM_CHANNELS)
+    for idx in range(5): #len(dataset)/ bs 
+        image = dataset[idx*bs: (idx+1)*bs][0].to(device) #could also calculate throught cuda
+        m = torch.tensor([torch.mean(image.permute(1 ,0 , 2, 3)[i]) for i in range(NUM_CHANNELS)])
+        std = torch.tensor([torch.std(image.permute(1 ,0 , 2, 3)[i]) for i in range(NUM_CHANNELS)])
+        for i in range(NUM_CHANNELS):
+            marr[i] += m[i]
+            stdarr[i] += std[i]
+        #m = torch(
+        #std = torch
+    return marr
+        
+           
+           
 #TODO: add augmentation classes
 
 
 class ImagenetteDataset(torch.utils.data.Dataset):
     def __init__(self, path, crop_size=224, train=True, augment=True, label_index=1):#TODO: check crop_size, train and augment
+        self.crop_size = crop_size
+        self.augment = augment
+        
         self.labels = label_index
         csv_path = os.path.join(path, 'noisy_imagenette.csv')
         csv_file = pd.read_csv(csv_path)
         self.path = path
-        self.images_path = df_csv['path'].values.tolist()
+        self.images_path = csv_file['path'].values.tolist()
         
     def __getitem__(self, index):
-        image_path = os.path.join(self.path, self.images_path[index])
-        label = classes[self.images_path[index].split('/')[1]]
-        #TODO: return image and label
+        if type(index) is int:
+            image_path = os.path.join(self.path, self.images_path[index])
+            label = classes[self.images_path[index].split('/')[1]]
+            image = Image.open(image_path).convert('RGB')
+            return self.transform(image), label 
+        elif type(index) is slice:
+            step = index.step if index.step is not None else 1
+            stop = index.stop if index.stop is not None else len(self)
+            start = index.start if index.start is not None else 0 
+            if step == 0:
+                raise ZeroDivisionError()
+            bs = int((stop - start)/step)
+            if bs == 1:
+                image_path = os.path.join(self.path, self.images_path[start])
+                label = classes[self.images_path[start].split('/')[1]]
+                image = Image.open(image_path).convert('RGB')
+                return self.transform(image), label 
+            else:
+                batch = torch.zeros(bs, 3, self.crop_size, self.crop_size) #3 is rgb- num of channels, crop_size could be changed to a tuple right now integer
+            labels = torch.zeros(bs)
+            for i in range(start, stop, step):
+                image_path = os.path.join(self.path, self.images_path[i])
+                label = classes[self.images_path[i].split('/')[1]]
+                image = Image.open(image_path).convert('RGB')
+                batch[i] += self.transform(image)
+                labels[i] += label
+            return batch, labels       
+        
+        
         
     def __len__(self):
         return len(self.images_path)
+        
+    def transform(self, image):
+        tran = torchvision.transforms.Compose([transforms.Resize(self.crop_size),
+                                                  transforms.CenterCrop(self.crop_size),
+                                                  transforms.ToTensor(),
+                                                  transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                       std=[0.229, 0.224, 0.225])])
+        
+        return tran(image)
+        pass #TODO: implement transformations based on augmentations, normalizations and cropping
---
+++ test.py
@@ -0,0 +1,34 @@
+from genericpath import getsize
+import os
+import re
+import sys
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import unittest
+import torch
+import torch.nn as nn
+import torchvision
+import json
+
+
+from dataset import ImagenetteDataset as datasetclass
+from dataset import get_statistics 
+
+
+seed = 42
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+plt.rcParams.update({'font.size': 12})
+test = unittest.TestCase()
+
+print(device)
+
+dataset = datasetclass(r"C:\Users\Admin\Documents\Technion\Tutorials\Advance Deep Learning\hw\imagenette2")
+
+img = dataset[0:1][0]
+
+print(img.shape)
+
+print(get_statistics(dataset))
+
+#plt.imsave(r"C:\Users\Admin\Documents\Technion\Tutorials\Advance Deep Learning\hw\hw1\temp.png", img.permute(1,2,0).numpy())
